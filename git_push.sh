691e88ff5f99301f58ff264b3eacf8a5e91b1d63
Dimitri Chassignol <contact@dimitrichassignol.fr>
2024-06-22 21:48:29 +0200

    Auto_project_Mardi_10h

---
+++ git_push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Se placer dans le répertoire de votre projet Git
+cd /volume1/homes/dimitrichassignol/Autres/bot
+
+# Ajouter, commit et pousser les modifications
+git add .
+git commit -m "Auto_project_Mardi_10h"
+git push origin main  # Assurez-vous de changer 'main' par votre branche principale si nécessaire
+
